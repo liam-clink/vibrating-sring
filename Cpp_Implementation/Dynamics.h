adbac11baa4197ce320449d0dcf5b91cb2cdf9f6
Liam <clink.6@osu.edu>
2021-03-27 19:26:19 -0400

    Changed Formatting
    Changed Print() from a macro to a variadic templated function, cleaned unnecessary commented debug code, and made most lines under 80 characters.

--- Cpp_Implementation/Dynamics.h
+++ Cpp_Implementation/Dynamics.h
@@ -5,7 +5,11 @@
 #include "Vector2D.h"
 #include "Spring.h"
 
-Vector2D Force(Vector2D& r_mass, Vector2D& r_springLeft, Spring& sp_left, Vector2D& r_springRight, Spring& sp_right)
+Vector2D Force(Vector2D& r_mass,
+               Vector2D& r_springLeft,
+               Spring&   sp_left,
+               Vector2D& r_springRight,
+               Spring&   sp_right)
 {
     Vector2D r_mass_left  = r_mass - r_springLeft;
     Vector2D r_mass_right = r_mass - r_springRight;
@@ -13,8 +17,10 @@ Vector2D Force(Vector2D& r_mass, Vector2D& r_springLeft, Spring& sp_left, Vector
     double len_sp_left  = sqrt(vec_inner(r_mass_left , r_mass_left ));
     double len_sp_right = sqrt(vec_inner(r_mass_right, r_mass_right));
 
-    Vector2D force_left  = r_mass_left  * (-sp_left. k() * (len_sp_left  - sp_left. L0()) / len_sp_left );
-    Vector2D force_right = r_mass_right * (-sp_right.k() * (len_sp_right - sp_right.L0()) / len_sp_right);
+    Vector2D force_left  = r_mass_left *
+        (-sp_left. k() * (len_sp_left  - sp_left. L0()) / len_sp_left );
+    Vector2D force_right = r_mass_right *
+        (-sp_right.k() * (len_sp_right - sp_right.L0()) / len_sp_right);
 
     return force_left + force_right;
 }
@@ -23,15 +29,21 @@ Vector2D Force(Vector2D& r_mass, Vector2D& r_springLeft, Spring& sp_left, Vector
 // ==========================================================================
 //
 
-void Update(std::vector<Vector2D>& positions, std::vector<Vector2D>& velocities, std::vector<Spring>& springs, std::vector<double>& masses, double delta_time)
+void Update(std::vector<Vector2D>& positions,
+            std::vector<Vector2D>& velocities,
+            std::vector<Spring>&   springs,
+            std::vector<double>&   masses,
+            double                 delta_time)
 {
-    // Print("Inside update\n");
     int N = (int) positions.size();
     for (int i = 1; i < N-1; i++) positions[i] = velocities[i] * delta_time;
     for (int i = 1; i < N-1; i++)
     {
-        Vector2D t_force = Force(positions[i], positions[i-1], springs[i-1], positions[i+1], springs[i]);
-        // Print(t_force);
+        Vector2D t_force = Force(positions[i],
+                                 positions[i-1],
+                                 springs[i-1],
+                                 positions[i+1],
+                                 springs[i]);
         velocities[i] += t_force * (delta_time / masses[i]);
     }
 }
@@ -40,7 +52,12 @@ void Update(std::vector<Vector2D>& positions, std::vector<Vector2D>& velocities,
 // ==========================================================================
 //
 
-void Update(std::vector<Vector2D>& positions, std::vector<Vector2D>& velocities, Spring& springs, double masses, double delta_time, bool parallel)
+void Update(std::vector<Vector2D>& positions,
+            std::vector<Vector2D>& velocities,
+            Spring&                springs,
+            double                 masses,
+            double                 delta_time,
+            bool                   parallel)
 {
     int N = (int) positions.size();
     if (parallel)   
@@ -54,7 +71,11 @@ void Update(std::vector<Vector2D>& positions, std::vector<Vector2D>& velocities,
         #pragma omp parallel for
         for (int i = 1; i < N-1; i++)
         {
-            Vector2D t_force = Force(positions[i], positions[i-1], springs, positions[i+1], springs);
+            Vector2D t_force = Force(positions[i],
+                                     positions[i-1],
+                                     springs,
+                                     positions[i+1],
+                                     springs);
             velocities[i-1] += t_force * (delta_time / masses);
         }
     }
@@ -67,7 +88,11 @@ void Update(std::vector<Vector2D>& positions, std::vector<Vector2D>& velocities,
 
         for (int i = 1; i < N-1; i++)
         {
-            Vector2D t_force = Force(positions[i], positions[i-1], springs, positions[i+1], springs);
+            Vector2D t_force = Force(positions[i],
+                                     positions[i-1],
+                                     springs,
+                                     positions[i+1],
+                                     springs);
             velocities[i-1] += t_force * (delta_time / masses);
         }
     }
