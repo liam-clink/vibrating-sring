df974ff888b38b97c0567a27675ab3e4206bb7e8
ominusliticus <elkevin111@gmail.com>
2021-03-27 18:21:18 -0400

    Changed parameters into struct

--- Cpp_Implementation/string_simulation.cpp
+++ Cpp_Implementation/string_simulation.cpp
@@ -11,7 +11,18 @@
 #define PARALLEL 1
 #define Print(x) std::cout << x << std::endl;
 
-void ReadParametersFromFile(const char*,int&,double&,double&,double&,double&,double&,bool&);
+struct Simulation_Paramters
+{
+    int num_masses;
+    double spring_length;
+    double spring_const;
+    double mass;
+    double elapsed_time;
+    double time_step;
+    bool parallel;
+};
+
+void ReadParametersFromFile(const char*, Simulation_Paramters& params);
 
 int main()
 {
@@ -19,42 +30,36 @@ int main()
     Print("For now we will assume that all springs and masses are the same.")
     Print(' ');
 
-    int num_masses {0};
-    double length {0};
-    double spring_const {0};
-    double mass {0};
-    double time {0};
-    double dt {0};
-    bool parallel {false};
-    ReadParametersFromFile("params.txt", num_masses, length, spring_const, mass, time, dt, parallel);
+    Simulation_Paramters params;
+    ReadParametersFromFile("params.txt", params);
 
     // setup variables
     Print("Setting up variable.");
-    std::vector<Vector2D> positions{};  positions. resize(num_masses + 2); // "+ 2" is for fixed endpoints
-    std::vector<Vector2D> velocities{}; velocities.resize(num_masses);
+    std::vector<Vector2D> positions{};  positions. resize(params.num_masses + 2); // "+ 2" is for fixed endpoints
+    std::vector<Vector2D> velocities{}; velocities.resize(params.num_masses);
 
     // std::vector<Spring> springs{}; springs.resize(num_masses + 1); // "+ 1" num_masses partitions into num_masses + 1 segments
 
     // std::vector<double> masses{}; masses.resize(num_masses);
 
-    Spring sp = Spring(spring_const, length);
+    Spring sp = Spring(params.spring_const, params.spring_length);
 
     // from end point to endpoint length
-    double full_length = (double) (num_masses + 1) * length;
+    double full_length = (double) (params.num_masses + 1) * params.spring_length;
 
     // set endpoints' positions 
     positions[0]              = Vector2D(0.         , 0.);
-    positions[num_masses + 1] = Vector2D(full_length, 0.);
+    positions[params.num_masses + 1] = Vector2D(full_length, 0.);
 
-    for (int i = 1; i <= num_masses; i++) positions[i] = Vector2D((double) i * length, sin((double) i * 3.14159 / 11));
+    for (int i = 1; i <= params.num_masses; i++) positions[i] = Vector2D((double) i * params.spring_length, sin((double) i * 3.14159 / 11));
 
     Print("Running simulation.");
     double t = 0;
     auto start = std::chrono::system_clock::now();
-    while(t < time)
+    while(t < params.elapsed_time)
     {
-        Update(positions, velocities, sp, mass, dt, parallel);
-        t += dt;
+        Update(positions, velocities, sp, params.mass, params.time_step, params.parallel);
+        t += params.time_step;
     }
     auto elapsed = std::chrono::system_clock::now() - start;
     Print(std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() << " ms" << std::endl);
@@ -71,8 +76,10 @@ int main()
 // ==========================================================================
 //
 
-void ReadParametersFromFile(const char* file_name, int& mass_count, double& spring_length, double& spring_const, double& mass, double& elapsed_time, double& time_step, bool& parallel)
+void ReadParametersFromFile(const char* file_name, Simulation_Paramters& params)
 {
+    // TODO: Implement variations that read in different numbrs of masses and spring constants
+    //       to facilitate, we can offer some templates.
     std::fstream fin(file_name, std::fstream::in); 
 
     if (!fin.is_open())
@@ -87,13 +94,13 @@ void ReadParametersFromFile(const char* file_name, int& mass_count, double& spri
         std::stringstream line(input);
         line >> input;
 
-        if      (input == "number_of_masses")   line >> mass_count;
-        else if (input == "equilibrium_length") line >> spring_length;
-        else if (input == "spring_constant")    line >> spring_const;
-        else if (input == "mass")               line >> mass;
-        else if (input == "total_elapsed_time") line >> elapsed_time;
-        else if (input == "time_step")          line >> time_step;
-        else if (input == "run_parallel")       line >> parallel;
+        if      (input == "number_of_masses")   line >> params.num_masses;
+        else if (input == "equilibrium_length") line >> params.spring_length;
+        else if (input == "spring_constant")    line >> params.spring_const;
+        else if (input == "mass")               line >> params.mass;
+        else if (input == "total_elapsed_time") line >> params.elapsed_time;
+        else if (input == "time_step")          line >> params.time_step;
+        else if (input == "run_parallel")       line >> params.parallel;
         else continue;
     }
 }
